8275dc5272d3cfbdd6daee1d1b562f8e897271fa
DenisZaychikov <38691003+DenisZaychikov@users.noreply.github.com>
2018-09-16 20:34:10 +0300

    Update nginx_log_parser.py

--- nginx_log_parser.py
+++ nginx_log_parser.py
@@ -1,25 +1,23 @@
 counter=0
 ip_list=[]
 
-with open('www.trendybelly.com.log') as file:
+with open('req.txt') as file:
     for line in file:
         line=line.strip()
-        ip_list.append(line[:line.find(' [')])
-        left_request_name=line.find('"')+1
-        right_request_name=line.find(' /')
-        name_request=line[left_request_name:right_request_name]
-        left_status_code=line.find('HTTP/1.1" ')+10
-        right_status_code=line.find('HTTP/1.1" ')+13
-        name_status_code=line[left_status_code:right_status_code]
-        if name_status_code=='500':
-            with open('requests_500.txt','a') as f:
-                f.write(line+'\n')
-        elif name_status_code=='200' and name_request=='GET':
+        parsed_line=line.split()
+        ip=parsed_line[0]
+        status_code=parsed_line[8]
+        request=parsed_line[5]
+        if ip not in ip_list:
+            ip_list.append(ip)
+        f=open('request_500.txt','a')
+        if status_code=='500':
+            f.write(line+'\n')
+        elif status_code=='200' and 'GET' in request:
             counter+=1
+    f.close()
 print(counter)
-print(*set(ip_list),sep=', ')
+print(', '.join(ip_list))
 
-    
-                         
-                 
+
 
